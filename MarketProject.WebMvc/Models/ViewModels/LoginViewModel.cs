936752874875c0969724399cd93a211344069458
beyzaaydogan01 <113782704+beyzaaydogan01@users.noreply.github.com>
2025-05-31 10:56:02 +0300

    login and register added

---
+++ MarketProject.WebMvc/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarketProject.WebMvc.Models.ViewModels;
+
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Kullanıcı adı gereklidir.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Şifre gereklidir.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+
+    }
+
---
+++ MarketProject.WebMvc/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarketProject.WebMvc.Models.ViewModels;
+
+public class RegisterViewModel
+{
+    [Required]
+    public string FirstName { get; set; }
+
+    [Required]
+    public string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Şifreler uyuşmuyor")]
+    public string ConfirmPassword { get; set; }
+}
