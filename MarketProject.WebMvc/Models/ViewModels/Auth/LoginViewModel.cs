00075ab33fc3a07f4d7ae1421bad4f9d96c0f2c4
beyzaaydogan01 <113782704+beyzaaydogan01@users.noreply.github.com>
2025-06-02 19:40:16 +0300

    Auth and login view model

---
+++ MarketProject.WebMvc/Models/ViewModels/Auth/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarketProject.WebMvc.Models.ViewModels.Auth;
+
+public class LoginViewModel
+{
+    [Required(ErrorMessage = "Kullanıcı adı gereklidir.")]
+    public string UserName { get; set; }
+
+    [Required(ErrorMessage = "Şifre gereklidir.")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    public bool RememberMe { get; set; }
+
+}
+
