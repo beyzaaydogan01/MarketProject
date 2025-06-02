3f984cdc14acd043f322f55059477b30c0ef3579
beyzaaydogan01 <113782704+beyzaaydogan01@users.noreply.github.com>
2025-06-02 19:40:48 +0300

    Products view model

---
+++ MarketProject.WebMvc/Models/ViewModels/Products/ProductAddViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace MarketProject.WebMvc.Models.ViewModels.Products;
+
+public class ProductAddViewModel
+{
+
+    [Required(ErrorMessage = "İsim Alanı Zorunludur.")]
+    [MinLength(2, ErrorMessage = "İsim Alanı Minimum 2 haneli olmalıdır.")]
+    public string Name { get; set; }
+
+    public decimal Price { get; set; }
+
+    public int Stock { get; set; }
+
+    public string? Description { get; set; }
+
+    public IFormFile? File { get; set; }
+
+    [Required(ErrorMessage = "Kategori seçimi zorunludur.")]
+    public int CategoryId { get; set; }
+    public List<SelectListItem> Categories { get; set; } = new();   
+
+}
---
+++ MarketProject.WebMvc/Models/ViewModels/Products/ProductUpdateViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace MarketProject.WebMvc.Models.ViewModels.Products;
+
+public class ProductUpdateViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public string? Description { get; set; }
+    public string? ImageUrl { get; set; }
+
+    [Required]
+    public int CategoryId { get; set; }
+    public List<SelectListItem> Categories { get; set; } = new();
+
+    public IFormFile? File { get; set; }
+}
