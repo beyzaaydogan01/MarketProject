ebd38c263d76693ffc057b4d4acbb4289fde2fed
beyzaaydogan01 <113782704+beyzaaydogan01@users.noreply.github.com>
2025-06-02 19:39:14 +0300

    Dependencies added

---
+++ MarketProject.Repository/RepositoryDependencies.cs
@@ -0,0 +1,23 @@
+﻿using MarketProject.Repository.Contexts;
+using MarketProject.Repository.Repositories.Abstracts;
+using MarketProject.Repository.Repositories.Concretes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MarketProject.Repository;
+
+public static class RepositoryDependencies
+{
+    public static IServiceCollection AddRepositoryDependencies(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<ISalesRepository, SalesRepository>();
+
+        services.AddDbContext<BaseDbContext>(options =>
+        options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        return services;
+    }
+}
---
+++ MarketProject.Service/ServiceDependencies.cs
@@ -0,0 +1,22 @@
+﻿using MarketProject.Service.Abstracts;
+using MarketProject.Service.Concretes;
+using MarketProject.Service.Helpers.Cloudinary;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace MarketProject.Service;
+
+public static class ServiceDependencies
+{
+    public static IServiceCollection AddServiceDependencies(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        services.AddScoped<IProductService, ProductService>();
+        services.AddScoped<ICategoryService, CategoryService>();
+        services.AddScoped<ISalesService, SalesService>();
+        services.AddScoped<IFileService, FileService>();
+
+        return services;
+    }
+}
